9275c6fc4d1ca42673d52656223cf8560f43a578
Shigaev Zamir <z.shigaev@g.nsu.ru>
2021-10-06 00:12:54 +0700

    Commit 3

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import main_win
 import plot_canvas as pc
 import numpy as np
 import math
+import random as rnd
 import matplotlib.pyplot as plt
 from PyQt5 import QtWidgets, QtGui
 
-POINTS = 100
+POINTS = 1000
 
 
 class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
@@ -25,6 +26,8 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
         self.frame3_layout = pc.Layout(self.frame_3, self.x, self.y)
         self.frame_3.setLayout(self.frame3_layout)
         self.btn_Add_func.clicked.connect(self.add_Function)
+        self.btn_Reset_signal.clicked.connect(self.reset_signal)
+        self.btn_Add_noise.clicked.connect(self.add_noise)
 
     def add_Function(self):
         func = self.comboBox_func.currentText()
@@ -36,11 +39,27 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
         self.pTE_list_func.setPlainText('')
         self.pTE_list_func.setPlainText(self.list_func)
         self.base_array.add_function(func, A, B)
-        self.frame1_layout.draw(self.base_array.get_arrays()[0], self.base_array.get_arrays()[1])
+        self.frame1_layout.draw(self.base_array)
         self.frame_1.setLayout(self.frame1_layout)
         # print(self.base_array.get_arrays())
         # print(func)
         # print("Успех")
+
+    def add_noise(self):
+        min = self.lineEdit_min.text()
+        max = self.lineEdit_max.text()
+        self.base_array.add_noise(min, max)
+        self.frame1_layout.draw(self.base_array)
+        self.frame_1.setLayout(self.frame1_layout)
+
+    def reset_signal(self):
+        self.base_array.reset_arrays()
+        self.frame1_layout.draw(self.base_array)
+        self.frame_1.setLayout(self.frame1_layout)
+        self.list_func = ''
+        self.pTE_list_func.setPlainText('')
+
+
 
 
 class Plot():
@@ -48,6 +67,10 @@ class Plot():
         global POINTS
         self.x_array = np.linspace(0, 10, num=POINTS)
         self.y_array = np.zeros(POINTS)
+        self.x_min = 0
+        self.x_max = 0
+        self.y_min = 0
+        self.y_max = 0
 
     def add_function(self, func, A, B):
         i = 0
@@ -55,6 +78,28 @@ class Plot():
             for m in self.x_array:
                 a = self.y_array[i] + float(A)*math.sin(float(B)*m)
                 self.y_array[i] = a
+                if m > self.x_max:
+                    self.x_max = m
+                if m < self.x_min:
+                    self.x_min = m
+                if a > self.y_max:
+                    self.y_max = a
+                if a < self.y_min:
+                    self.y_min = a
+                i += 1
+            pass
+        elif func == 'Acos(Bx)':
+            for m in self.x_array:
+                a = self.y_array[i] + float(A)*math.cos(float(B)*m)
+                self.y_array[i] = a
+                if m > self.x_max:
+                    self.x_max = m
+                if m < self.x_min:
+                    self.x_min = m
+                if a > self.y_max:
+                    self.y_max = a
+                if a < self.y_min:
+                    self.y_min = a
                 i += 1
             pass
         else:
@@ -65,12 +110,22 @@ class Plot():
     #     self.y_array.append(a)
     # pass
 
+    def add_noise(self, min, max):
+        i = 0
+        for m in self.y_array:
+            noise = rnd.uniform(float(min), float(max))
+            self.y_array[i] = m + noise
+            i += 1
+            print(noise)
+
+
+
+    def reset_arrays(self):
+        self.x_array = np.linspace(0, 10, num=POINTS)
+        self.y_array = np.zeros(POINTS)
+
     def get_arrays(self):
         return self.x_array, self.y_array
-
-
-
-
 
 
 if __name__ == '__main__':
--- plot_canvas.py
+++ plot_canvas.py
@@ -10,22 +10,28 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 
+
+
+
+
 class Layout(QVBoxLayout):
     def __init__(self,root, x, y):
         super().__init__()
-        plt.style.use("seaborn-dark")
+        plt.style.use("classic")
         self.x = x
         self.y = y
         self.figure = plt.figure()
-        self.ax = self.figure.add_axes([0, 0, 1, 1])
+        self.ax = self.figure.add_axes([0.05, 0.1, 0.93, 0.85])
+        self.ax.grid()
         self.ax.plot(x, y)
         self.canvas = FigureCanvas(self.figure)
         self.addWidget(self.canvas)
 
-    def draw(self, new_x, new_y):
+    def draw(self, base_array):
         self.figure.clear()
-        self.ax = self.figure.add_subplot(111)
-        self.x = new_x
-        self.y = new_y
+        self.ax = self.figure.add_axes([0.05, 0.1, 0.93, 0.85])
+        self.ax.grid()
+        self.x = base_array.get_arrays()[0]
+        self.y = base_array.get_arrays()[1]
         self.ax.plot(self.x, self.y)
         self.canvas.draw()
