ae85ae1cea86260fc46bf87a8a5b25aa34bc85fe
Zamir Shigaev <z.shigaev@g.nsu.ru>
2021-10-06 13:37:34 +0700

    Commit 4

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import math
 import random as rnd
 import matplotlib.pyplot as plt
 from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtGui import QDoubleValidator as QDV
+from PyQt5.QtGui import QIntValidator as QIV
 
 POINTS = 1000
 
@@ -25,9 +27,17 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
         self.frame_2.setLayout(self.frame2_layout)
         self.frame3_layout = pc.Layout(self.frame_3, self.x, self.y)
         self.frame_3.setLayout(self.frame3_layout)
+        # Сигналы кнопок
         self.btn_Add_func.clicked.connect(self.add_Function)
         self.btn_Reset_signal.clicked.connect(self.reset_signal)
         self.btn_Add_noise.clicked.connect(self.add_noise)
+        # Сигналы виджетов
+        self.lineEdit_min_time.textEdited.connect(self.update_plot)
+        self.lineEdit_max_time.textEdited.connect(self.update_plot)
+        # Настройки виджетов
+        # self.lineEdit_min.setValidator(QIV(0, 9))
+        # self.lineEdit_min.setValidator(QDV(1, 9999, 4, self))
+
 
     def add_Function(self):
         func = self.comboBox_func.currentText()
@@ -38,7 +48,9 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
         self.list_func = self.list_func + str_func + "   "
         self.pTE_list_func.setPlainText('')
         self.pTE_list_func.setPlainText(self.list_func)
-        self.base_array.add_function(func, A, B)
+        min = float(self.lineEdit_min_time.text())
+        max = float(self.lineEdit_max_time.text())
+        self.base_array.add_function(func, A, B, min, max)
         self.frame1_layout.draw(self.base_array)
         self.frame_1.setLayout(self.frame1_layout)
         # print(self.base_array.get_arrays())
@@ -47,7 +59,13 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
 
     def add_noise(self):
         min = self.lineEdit_min.text()
+        min_degree = self.comboBox_min.currentText()
+        min = float(min) * 10 ** float(min_degree)
+        # print(min)
         max = self.lineEdit_max.text()
+        max_degree = self.comboBox_max.currentText()
+        max = float(max) * 10 ** float(max_degree)
+        # print(max)
         self.base_array.add_noise(min, max)
         self.frame1_layout.draw(self.base_array)
         self.frame_1.setLayout(self.frame1_layout)
@@ -59,6 +77,21 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
         self.list_func = ''
         self.pTE_list_func.setPlainText('')
 
+    def update_plot(self):
+        if self.lineEdit_min_time.text() == '':
+            pass
+        elif self.lineEdit_min_time.text() == '-':
+            pass
+        elif self.lineEdit_max_time.text() == '':
+            pass
+        elif self.lineEdit_max_time.text() == '-':
+            pass
+        else:
+            min = float(self.lineEdit_min_time.text())
+            max = float(self.lineEdit_max_time.text())
+            self.base_array.rewrite_arrays(min=min, max=max)
+            self.frame1_layout.draw(self.base_array)
+            self.frame_1.setLayout(self.frame1_layout)
 
 
 
@@ -71,12 +104,42 @@ class Plot():
         self.x_max = 0
         self.y_min = 0
         self.y_max = 0
+        self.array_functions = []
+        self.old_min_time = 0
+        self.old_max_time = 10
 
-    def add_function(self, func, A, B):
+    def add_function(self, func, A, B, min, max):
+        self.array_functions.append([func, A, B])
+        if self.old_min_time != min or self.old_max_time != max:
+            self.old_min_time = min
+            self.old_max_time = max
+            self.rewrite_arrays()
+        else:
+            self.add_one_function(func, A, B)
+        # self.array_functions.append([func, A, B])
+
+        # print(self.y_array)
+    # for m in self.x_array:
+    #     a = math.sin(m)
+    #     self.y_array.append(a)
+    # pass
+
+    def rewrite_arrays(self, min=None, max=None):
+        if min != None:
+            if max != None:
+                self.old_min_time = min
+                self.old_max_time = max
+        self.x_array = np.linspace(self.old_min_time, self.old_max_time, num=POINTS)
+        self.y_array = np.zeros(POINTS)
+        for m in self.array_functions:
+            self.add_one_function(m[0], m[1], m[2])
+
+    def add_one_function(self, func, A, B):
+        # self.x_array = np.linspace(min, max, num=POINTS)
         i = 0
         if func == 'Asin(Bx)':
             for m in self.x_array:
-                a = self.y_array[i] + float(A)*math.sin(float(B)*m)
+                a = self.y_array[i] + float(A) * math.sin(float(B) * m)
                 self.y_array[i] = a
                 if m > self.x_max:
                     self.x_max = m
@@ -90,7 +153,7 @@ class Plot():
             pass
         elif func == 'Acos(Bx)':
             for m in self.x_array:
-                a = self.y_array[i] + float(A)*math.cos(float(B)*m)
+                a = self.y_array[i] + float(A) * math.cos(float(B) * m)
                 self.y_array[i] = a
                 if m > self.x_max:
                     self.x_max = m
@@ -104,11 +167,6 @@ class Plot():
             pass
         else:
             pass
-        print(self.y_array)
-    # for m in self.x_array:
-    #     a = math.sin(m)
-    #     self.y_array.append(a)
-    # pass
 
     def add_noise(self, min, max):
         i = 0
@@ -116,12 +174,10 @@ class Plot():
             noise = rnd.uniform(float(min), float(max))
             self.y_array[i] = m + noise
             i += 1
-            print(noise)
-
-
+            # print(noise)
 
     def reset_arrays(self):
-        self.x_array = np.linspace(0, 10, num=POINTS)
+        self.x_array = np.linspace(self.old_min_time, self.old_max_time, num=POINTS)
         self.y_array = np.zeros(POINTS)
 
     def get_arrays(self):
---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x = np.linspace(1, 10, 10)
+x = np.linspace(-10, -1, 10)
+
+print(x)
